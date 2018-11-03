1557d349bfed105266a85aac7fe63d405be2fd53
卢洁辉 <lujiehui@appledeMacBook-Pro.local>
2018-11-03 15:31:50 +0800

    5

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const Koa = require('koa')
 const app = new Koa()
+const views = require('koa-views')
+const {resolve} = require('path')
+
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 app.use(async (ctx, next) => {
-	ctx.body = '电影首页'
+  await ctx.render('index', {
+    you: 'jld',
+    me: 'ljh'
+  })
 })
 app.listen(4455)
